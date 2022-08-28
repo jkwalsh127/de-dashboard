dbb93994ef83f7df5d99e80ccfa2ffe2279495d6
Jake Walsh <jkwalsh127@gmail.com>
2022-08-28 02:37:56 -0700

    fix bugs preventing build

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import MainContent from "./components/MainContent/MainContent";
 import SupportingContent from "./components/SupportingConent/SupportingContent";
 import Header from "./components/Header/Header";
-import { DataStore } from '@aws-amplify/datastore';
-import { Users } from './models';
+// import { DataStore } from '@aws-amplify/datastore';
+// import { Users } from './models';
 
 
 export default function App() {
@@ -14,9 +14,9 @@ export default function App() {
   }, []);
 
   async function fetchUser() {
-    const userModels = await DataStore.query(Users);
-    console.log(userModels);
-    setUser(userModels);
+    // const userModels = await DataStore.query(Users);
+    // console.log(userModels);
+    // setUser(userModels);
   }
 
   const [supporting, setSupporting] = useState("Overview");

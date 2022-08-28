0b4464f3c5a758ec8a7acb0a52a42dcec02aaef5
Jake Walsh <jkwalsh127@gmail.com>
2022-08-27 23:19:19 -0700

    initiate grid for supporting content container

--- src/components/SupportingConent/SupportingOverview/SupportingOverview.js
+++ src/components/SupportingConent/SupportingOverview/SupportingOverview.js
@@ -1,13 +1,25 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
 export default function SupportingOverview() {
 
 
     return (
-        <div className="details-list">
-            <ul>
-                <li>Hello</li>
-            </ul>
-        </div>
+        <>
+            <div className="detail-1">
+                <Typography>Your Investment</Typography>
+                <Typography>$100,000</Typography>
+            </div>
+            <div className="detail-2">
+                <Typography>Your Balance</Typography>
+                <Typography>$131,254</Typography>
+            </div>
+            <div className="detail-3">
+                <Typography>Profit</Typography>
+            </div>
+            <div className="detail-4">
+                <Typography>Withdrawls</Typography>
+            </div>
+        </>
     )
 }
